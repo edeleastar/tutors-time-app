8f6fd9cf351e9c2a11ccae547b1408bd8d76b09c
Eamonn de Leastar <edeleastar@wit.ie>
2026-02-12 08:39:40 +0000

    lab route

---
+++ src/routes/[courseid]/lab/bylab/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import LabsComponent from "$lib/components/labs/LabsComponent.svelte";
+  import { page } from "$app/stores";
+
+  const courseId = $derived(($page.params.courseid as string) ?? "");
+</script>
+
+{#key courseId}
+  <LabsComponent courseId={courseId} mode="lab" />
+{/key}
---
+++ src/routes/[courseid]/lab/median/byweek/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import LabsMedianComponent from "$lib/components/labs/LabsMedianComponent.svelte";
+  import { page } from "$app/stores";
+
+  const courseId = $derived(($page.params.courseid as string) ?? "");
+</script>
+
+{#key courseId}
+  <LabsMedianComponent courseId={courseId} mode="week" />
+{/key}
